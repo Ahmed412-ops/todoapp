8c9a74c5bfe9dd88b8292968186a9568c2634c06
Ahmed <ahmed.mostafa.elsayed45@gmail.com>
2026-05-15 22:38:24 +0300

    connect fastapi with postgresql

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from app.database.database import engine, Base
+from app.models.user import User
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
